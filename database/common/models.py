a247354e94d6d80454d7ee547b47d228dffe8b49
Timur_Khamidullin <timur.ivanov28@mail.ru>
2024-04-08 17:26:08 +0500

    new basic changes

---
+++ database/common/models.py
@@ -0,0 +1,50 @@
+from config_data.config import DB_PATH
+from peewee import CharField, DateField, ForeignKeyField, IntegerField, Model, SqliteDatabase
+
+db = SqliteDatabase(DB_PATH)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    user_id = IntegerField(primary_key=True)
+    username = CharField()
+    first_name = CharField()
+    last_name = CharField(null=True)
+
+
+class History(BaseModel):
+    user = ForeignKeyField(User, backref="history")
+    title = CharField()
+    created_at = DateField()
+
+    def __str__(self):
+        return '{date} - {film_title}'.format(
+            date=self.created_at,
+            film_title=self.title
+        )
+
+
+class Movie:
+    def __init__(self, name, year, country, genre, rating_kp, rating_imdb, description, poster_url):
+        self.name = name,
+        self.year = year,
+        self.country = country,
+        self.genre = genre,
+        self.rating_kp = rating_kp,
+        self.rating_imdb = rating_imdb,
+        self.description = description,
+        self.poster_url = poster_url
+
+    def __str__(self):
+        text = f'Информация о фильме: \n'\
+                f'Название: {str(*self.name)}\nГод: {int(*self.year)}\nСтрана: {str(*self.country)}\n'\
+                f'Жанр: {str(*self.genre)}\nРейтинг КП: {float(*self.rating_kp)}\n'\
+                f'Рейтинг IMDb: {float(*self.rating_imdb)}\n'\
+                f'Краткое описание: {str(*self.description)}\nПостер: {self.poster_url}\n'
+
+        return text
+
